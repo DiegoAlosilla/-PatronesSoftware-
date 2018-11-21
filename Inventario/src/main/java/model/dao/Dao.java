f8595db7ac6779098262dbf97bcdafcffa5946c6
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2018-11-21 01:38:12 -0500

    Codigo de DAO

--- Inventario/src/main/java/model/dao/Dao.java
+++ Inventario/src/main/java/model/dao/Dao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
-public class Dao {
+import java.util.List;
 
+public interface Dao<T> {
+	T find(int id);
+
+	boolean create(T obj);
+
+	boolean update(T obj);
+
+	boolean delete(List<Integer> ids);
+
+	List<T> getAll();
 }
