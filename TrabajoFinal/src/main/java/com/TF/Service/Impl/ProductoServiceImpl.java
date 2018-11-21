e9ee019bb518189cef68a8f2f0fde6afa2c9cd74
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2018-11-21 13:52:37 -0500

    ProductoService
    Solo las clases e interfaces

---
+++ TrabajoFinal/src/main/java/com/TF/Service/Impl/ProductoServiceImpl.java
@@ -0,0 +1,7 @@
+package com.TF.Service.Impl;
+
+import com.TF.Service.ProductoService;
+
+public class ProductoServiceImpl implements ProductoService{
+
+}
---
+++ TrabajoFinal/src/main/java/com/TF/Service/ProductoService.java
@@ -0,0 +1,7 @@
+package com.TF.Service;
+
+import com.TF.Model.Dto.Producto;
+
+public interface ProductoService extends CRUDservice<Producto>{
+
+}
