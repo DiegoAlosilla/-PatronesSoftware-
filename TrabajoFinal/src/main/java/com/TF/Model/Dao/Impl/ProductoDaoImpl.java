3251cbc00170f062e2f764fe0a7becb1ab2f24a3
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2018-11-21 15:06:07 -0500

    ProductoServiceImpl
    Producto service impl implementado

--- TrabajoFinal/src/main/java/com/TF/Model/Dao/Impl/ProductoDaoImpl.java
+++ TrabajoFinal/src/main/java/com/TF/Model/Dao/Impl/ProductoDaoImpl.java
@@ -2,12 +2,15 @@ package com.TF.Model.Dao.Impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.TF.Model.Dao.ProductoDao;
 import com.TF.Model.DataBase.DataBaseMysql;
 import com.TF.Model.Dto.Producto;
+import com.TF.Model.Dto.Proveedor;
 
 
 
@@ -39,26 +42,81 @@ public class ProductoDaoImpl implements ProductoDao{
 
 	@Override
 	public boolean Delete(Producto obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			cn = db.connectDb();
+			PreparedStatement prepare = cn.prepareStatement("DELETE FROM productos WHERE id =?");
+			prepare.setInt(1, obj.getId());
+			prepare.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		} finally {
+			db.desconnectDb();
+		}
 	}
 
 	@Override
 	public boolean Update(Producto obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			cn = db.connectDb();
+			PreparedStatement prepare = cn.prepareStatement("UPDATE productos SET nombre=? , precio=? , stock=?, unidad=? where id=?");
+			prepare.setString(1, obj.getNombre());
+			prepare.setDouble(2, obj.getPrecio());
+			prepare.setInt(3, obj.getStock());
+			prepare.setInt(4, obj.getUnidad());
+			prepare.setInt(5, obj.getId());
+			prepare.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		} finally {
+			db.desconnectDb();
+		}
 	}
 
 	@Override
 	public List<Producto> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Producto producto = null;
+		List<Producto> listado = new ArrayList<>();
+		try {
+			cn = db.connectDb();
+			PreparedStatement prepare = cn.prepareStatement("SELECT p.id, p.nombre, p.precio, p.stock, p.unidad FROM productos p");
+			ResultSet result = prepare.executeQuery();
+			while(result.next()) {
+				producto = new Producto();
+				producto.setId(result.getInt("id"));
+				producto.setNombre(result.getString("razon"));
+				producto.setPrecio(result.getDouble(("rubro")));
+				producto.setStock(result.getInt("ruc"));
+				listado.add(producto);
+			}
+			return listado;
+		} catch(SQLException e) {
+			return null;
+		} finally {
+			db.desconnectDb();
+		}
 	}
 
 	@Override
 	public Producto FindBy(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Producto producto = null;
+		try {
+			cn = db.connectDb();
+			PreparedStatement prepare = cn.prepareStatement("SELECT p.id, p.nombre, p.precio, p.stock, p.unidad FROM productos p where p.id=?");
+			prepare.setInt(1, id);
+			ResultSet result = prepare.executeQuery();
+			producto = new Producto();
+			producto.setId(result.getInt("id"));
+			producto.setNombre(result.getString("razon"));
+			producto.setPrecio(result.getDouble(("rubro")));
+			producto.setStock(result.getInt("ruc"));
+			return producto;
+		} catch(SQLException e) {
+			return null;
+		} finally {
+			db.desconnectDb();
+		}
 	}
 
 }
--- TrabajoFinal/src/main/java/com/TF/Service/Impl/ProductoServiceImpl.java
+++ TrabajoFinal/src/main/java/com/TF/Service/Impl/ProductoServiceImpl.java
@@ -1,7 +1,40 @@
 package com.TF.Service.Impl;
 
+import java.util.List;
+
+import com.TF.Model.Dto.Producto;
 import com.TF.Service.ProductoService;
 
 public class ProductoServiceImpl implements ProductoService{
+
+	@Override
+	public boolean Save(Producto obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Delete(Producto obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Update(Producto obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Producto> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Producto FindBy(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
