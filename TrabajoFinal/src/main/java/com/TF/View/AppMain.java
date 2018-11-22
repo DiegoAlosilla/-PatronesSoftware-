00936d7ab15342007473458a5be71e87f22a11d1
Maximo Mallqui Tertuliano <35297513+maximiliano235@users.noreply.github.com>
2018-11-21 21:23:29 -0500

    Main
    Actualizado el main

--- TrabajoFinal/src/main/java/com/TF/View/AppMain.java
+++ TrabajoFinal/src/main/java/com/TF/View/AppMain.java
@@ -79,6 +79,43 @@ public class AppMain {
                     //Ingresar numero de orden
                     System.out.println("Codigo    Proveedor             Precio     Descuento      Importe ");
                     break;
+                case 3:
+                	System.out.println("Modificar Producto ");
+                    System.out.println("------------------ ");
+                    System.out.println("Si desea modificar ingrese el nuevo valor, si no desea modificar ingrese 0 ");
+                    System.out.println("Nombre :  ");
+                    //Ingresar variable nombre
+                    System.out.println("Precio :  ");
+                    //Ingresar precio
+                    System.out.println("Nivel minimo : ");
+                    //Ingresar nivel minimo
+                    System.out.println("Proveedor : ");
+                    //Ingresar Proveedor 
+                    System.out.println("Producto Modificado");
+                    break;
+                case 4:
+                    System.out.println("Consultar Proveedor");
+                    System.out.println("-------------------");
+                    System.out.println("RUC(Finalize con enter) : ");
+                    System.out.println("Codigo       Razon Social   RUC    Rubro   Direccion");
+
+                    //ingresa ruc
+                    break;
+                case 5:
+                    System.out.println("Actualizar estado de cotizacion");
+                    System.out.println("-------------------------------");
+                    System.out.println("Codigo(Finalize con enter) : ");
+                    //Ingresa el codigo
+                    System.out.println("Codigo     Proveedor    Precio   Descuento");
+                    System.out.println("Nombre : ");
+                    //Mostrar nombre del proveedor
+                    System.out.println("Estado de cotización actualizado ");
+                    break;
+                default:
+                    break;
+            }
+        } while (op != 1 && op != 2 && op != 3 && op != 4 && op != 5);
+	}
             
             /*Proveedor obj;
 		List<Proveedor> listadoP;
@@ -165,4 +202,4 @@ public class AppMain {
 		s.close();
 	}
 
-}
+
